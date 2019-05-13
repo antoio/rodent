0c7867e1ed17d2ca373cef136358e73147ff33fb
Antonio Galluccio <antonio.galluccio@emineo.ch>
2019-05-13 20:05:51 +0200

    add middleware

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,29 @@
 import express from 'express';
 import router from './routes/person.mjs';
+import * as path from 'path';
 
 let app = express();
 
+// Middleware
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`);
+    next();
+});
 app.use(router);
 app.use(express.static('public'));
+
+// Handler for 404 - Resource not found
+app.use((req, res, next) => {
+    res.status(404).send('Where do you think you\'re going?');
+});
+
+// Handler for 500
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    console.log(process.cwd());
+
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+});
 
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
