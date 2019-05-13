e2121188413e42af9bbac879b638c71a74c27325
Antonio Galluccio <antonio.galluccio@emineo.ch>
2019-05-13 21:35:08 +0200

    db connection to mongo db cloud

--- src/services/dbservice.mjs
+++ src/services/dbservice.mjs
@@ -1,15 +1,24 @@
 import * as mongo from 'mongodb';
 
-const client = mongo.default.MongoClient;
-const url = 'mongodb://localhost:27017';
+const testdbname = 'clusterfsck-kcjcx.mongodb.net';
+const username = 'budb';
+const password = 'b00mb00m';
+
+const url = `mongodb+srv://${username}:${password}@${testdbname}/test?retryWrites=true`;
 
 export default function start(){
     console.log("Running...");
-    client.connect(url, (err, db) => {
+    const client = new mongo.default.MongoClient(url, {useNewUrlParser: true});
+    client.connect((err, db) => {
         if(err) {
             console.error(err);
             return;
         }
+        const client_db = client.db("test");
+        //console.log(client_db);
+
+        const coll = client_db.collection("devices");
+        console.log(coll);
 
         console.log("Success 🎉");
         db.close();
