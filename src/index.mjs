9c566729b2c3a9e7fd14e0d2393cbb588a3b8fd0
Antonio Galluccio <antonio.galluccio@emineo.ch>
2019-05-13 20:13:03 +0200

    add __filename and __dirname for ES Modules

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,13 @@
 import express from 'express';
 import router from './routes/person.mjs';
-import * as path from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 let app = express();
+const __filename = fileURLToPath(import.meta.url);
+console.log(`__filename: ${__filename}`);
+const __dirname = path.dirname(__filename);
+console.log(`__dirname: ${__dirname}`);
 
 // Middleware
 app.use((req, res, next) => {
